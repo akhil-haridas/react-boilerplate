e434da5a1fad9960c475af9a92f3d811a604a4ee
akhil-haridas <4khilharidas@gmail.com>
2024-07-18 17:48:19 +0530

    private route

---
+++ src/components/auth/PrivateRoute.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Redirect, Route, RouterProps } from 'react-router-dom';
+
+const isAuthenticated = () => { }
+
+interface PrivateRouteProps extends RouterProps {
+    layout: React.ComponentType<any>;
+    component: React.ComponentType<any>;
+}
+
+const PrivateRoute: React.FC<PrivateRouteProps> = ({
+    layout: Layout,
+    component: Component,
+    ...rest
+}) => (
+    <Route
+        {...rest}
+        render={(props) =>
+            isAuthenticated() ? (
+                <Layout>
+                    <Component {...props} />
+                </Layout>
+            ) : (
+                <Redirect to="/login" />
+            )
+        }
+    />
+);
+
+export default PrivateRoute;
---
+++ src/routes/routes.tsx
@@ -0,0 +1,17 @@
+// src/routes.tsx
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import PrivateRoute from './components/auth/PrivateRoute';
+
+const Routes: React.FC = () => (
+    <Switch>
+        <Route exact path="/" component={<></>} />
+        <Route path="/login" component={<></>} />
+        <Route path="/signup" component={<></>} />
+        <PrivateRoute path="/chat" layout={<></>} component={<></>} />
+        <PrivateRoute path="/profile" layout={<></>} component={<></>} />
+        <Route component={<></>} />
+    </Switch>
+);
+
+export default Routes;
