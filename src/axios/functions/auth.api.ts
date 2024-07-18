8870d69fafd8efd3bcb23cc494a6901d28d8b780
akhil-haridas <4khilharidas@gmail.com>
2024-07-18 17:06:19 +0530

    set axios endpoint as unique

--- src/axios/functions/auth.api.ts
+++ src/axios/functions/auth.api.ts
@@ -1,3 +1,4 @@
+import { LOGIN_ENDPOINT, REGISTER_ENDPOINT } from "../../constants/auth/constants";
 import apiClient from "../axiosClient";
 
 interface LoginData {
@@ -12,13 +13,13 @@ interface RegisterData {
 }
 
 export const login = async (data: LoginData) => {
-    const response = await apiClient.post('/auth/login', data);
+    const response = await apiClient.post(LOGIN_ENDPOINT, data);
     localStorage.setItem('token', response.data.token);
     return response.data;
 };
 
 export const register = async (data: RegisterData) => {
-    const response = await apiClient.post('/auth/register', data);
+    const response = await apiClient.post(REGISTER_ENDPOINT, data);
     return response.data;
 };
 
